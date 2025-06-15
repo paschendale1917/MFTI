6262508757c436e947206150a6609ea4e6e6358f
Alexander Biryukov <bisiro@mail.ru>
2025-06-15 15:05:08 +0300

    first commit

--- base_C/exercise_G/G16.c
+++ base_C/exercise_G/G16.c
@@ -1,5 +1,5 @@
-// В файле input.txt дано предложение. Необходимо заменить все имена «Cao» на
-// «Ling» и записать результат в файл output.txt.
+// В файле input.txt дано предложение. Необходимо заменить все имена «Ling» на
+// «Cao» и результат записать в файл output.txt.
 
 #include <math.h>
 #include <stdint.h>
@@ -15,7 +15,6 @@ char *file_out = "output.txt";
 char output[MAX_CHAR] = "";
 char string[MAX_CHAR] = "";
 
-
 uint16_t cao_counter(char *str);
 void exchange(char *str_in, char *str_out);
 
@@ -30,41 +29,30 @@ int main(void) {
   return 0;
 }
 
-// uint16_t cao_counter(char *str) {
-//   uint16_t cntr = 0;
-//   uint16_t cao_cntr = 0;
-//   while (*(str + cntr) != '\000') {
-//     if (*(str + cntr) == 'C') {
-//       if (*(str + cntr + 1) == 'a') {
-//         *(str + cntr + 2) == 'o' ? cao_cntr++ : cao_cntr;
-//       }
-//     }
-//     cntr++;
-//   }
-//   return cao_cntr;
-// }
-
-uint8_t payload_finder(char *str) {
-  uint16_t cao = 0;
-  if (*str == 'C') {
-    if (*(str + 1) == 'a') {
-      *(str + 2) == 'o' ? cao++ : cao;
+uint8_t payload_finder(char *str, char *payload, uint8_t payload_size) {
+  uint8_t cntr = 0;
+  while (cntr < payload_size) {
+    if (*(str + cntr) == *(payload + cntr)) {
+      cntr++;
+    } else {
+      break;
     }
   }
-  return cao ? 1 : 0;
+  return cntr == payload_size ?  1 : 0;
 }
 
 void exchange(char *str_in, char *str_out) {
   uint16_t char_in = 0;
   uint16_t char_out = 0;
-  char *payload_out = "Ling";
+  char *payload_out = "Cao";
+  char *payload_in = "Ling";
   while (*(str_in + char_in) != '\000') {
-    if (payload_finder(str_in + char_in)) {
-      for (uint8_t u = 0; u < 4; u++) {
+    if (payload_finder(str_in + char_in, payload_in,strlen(payload_in))) {
+      for (uint8_t u = 0; u < strlen(payload_out); u++) {
         *(str_out + u + char_out) = *(payload_out + u);
       }
-      char_out += 4;
-      char_in += 3;
+      char_out += strlen(payload_out);
+      char_in += strlen(payload_in);
     } else {
       *(str_out + char_out) = *(str_in + char_in);
       char_in++;
